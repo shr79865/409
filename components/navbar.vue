241a4f1a001f02db0bd334010a388995ef14345d
shr79865 <n.shr79865@gmail.com>
2024-07-09 20:40:47 +0545

    409

---
+++ components/navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="flex justify-between bg-[#b0caf5b7] px-[80px] py-[20px] shadow-b-xl">
+
+        <div class="flex gap-5 items-center">
+            <img class="rounded-full" src="../public/410_logo_img.webp" alt="logo" width="30">
+
+            <div class="relative">
+                <input class="w-[250px] px-[20px] pr-[38px] py-[7px] rounded-full focus:outline-none" type="text"
+                    placeholder="Search">
+                <button class="absolute text-[20px] top-[3px] right-[10px] font-semibold hover:text-[#5578ec]"><i
+                        class="ri-search-line"></i></button>
+            </div>
+
+            <nuxt-link
+                class="bg-gradient-to-r from-pink-500 to-red-500 font-bold hover:scale-[1.1] shadow-lg hover:shadow-2xl text-[17px] duration-300 text-[#ffffff] px-[15px] py-[7px] rounded-full"
+                to="/Uevents">Upcoming Events</nuxt-link>
+
+        </div>
+
+
+
+        <div class="flex gap-[40px] font-semibold text-[18px] items-center">
+            <nuxt-link
+                class="text-[#354fc2] duration-300 hover:shadow-2xl hover:text-[#ffffff] hover:bg-gradient-to-r from-sky-500 to-indigo-500 hover:rounded-full hover:px-[15px] hover:py-[5px]"
+                to="/">HOME</nuxt-link>
+            <nuxt-link
+                class="text-[#354fc2] duration-300 hover:shadow-2xl hover:text-[#ffffff] hover:bg-gradient-to-r from-sky-500 to-indigo-500 hover:rounded-full hover:px-[15px] hover:py-[5px]"
+                to="/connect">CONNECT</nuxt-link>
+            <nuxt-link
+                class="text-[#354fc2] duration-300 hover:shadow-2xl hover:text-[#ffffff] hover:bg-gradient-to-r from-sky-500 to-indigo-500 hover:rounded-full hover:px-[15px] hover:py-[5px]"
+                to="/about">ABOUT</nuxt-link>
+            <nuxt-link
+                class="text-[#354fc2] duration-300 hover:shadow-2xl hover:text-[#ffffff] hover:bg-gradient-to-r from-sky-500 to-indigo-500 hover:rounded-full hover:px-[15px] hover:py-[5px]"
+                to="/contact">CONTACT</nuxt-link>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <div
+        class="flex pr-[110px] gap-7 justify-between items-center h-[660px] bg-gradient-to-r from-sky-500 to-indigo-500">
+
+        <!-- welcome section start -->
+        <div
+            class="flex flex-col justify-center items-center border-r-2 border-y-2 border-[#ffffffb2] h-[550px] rounded-r-full pl-[90px] pr-[150px]">
+
+            <h1 class="text-[#ffffff] font-bold text-[50px]">WELCOME TO <span
+                    class="text-[#91ffeb] text-[65px]">GrowX</span></h1>
+            <p class="text-[#ffffff] pb-[23px] text-[18px]">- Where innovation meets excellence</p>
+
+            <div class="flex justify-center items-center bg-[#ffffffb2] h-[230px] w-[580px] rounded-[35px]">
+                <p class="text-[#2c58a5] text-justify font-semibold px-[40px] py-[50px] text-[16px]">We empower startups
+                    by
+                    connecting them with essential funding and expert support from our vast
+                    network of investors. Our mission is to transform innovative concepts into thriving businesses,
+                    ensuring
+                    every entrepreneur has the resources and guidance needed to succeed. Join us on this journey of
+                    growth
+                    and
+                    innovation, and let's build the future together.</p>
+
+            </div>
+
+        </div>
+        <!-- welcome section end -->
+
+
+        <!-- share section start -->
+        <div>
+
+            <h1>Share Your Idea</h1>
+
+        </div>
+        <!-- share section end -->
+
+    </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
